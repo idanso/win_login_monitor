81887957389cada12112e84a470f3d2642f1da3e
idanso <sorany123@gmail.com>
2021-08-03 15:47:13 +0300

    Update VMUptime_Querys.sql

--- VMUptime_Querys.sql
+++ VMUptime_Querys.sql
@@ -69,28 +69,29 @@ order by VMName, grp;
 #### final query for statistic for persistent (Final) ####
 ##########################################################
 
-SELECT substring_index(substring_index(Logins_logs.VMName, '-(', -1),')-', 1) AS 'Owner',
+SELECT substring_index(substring_index(mod_reservations.VMName, '-(', -1),')-', 1) AS 'Owner',
 mod_reservations.Lab,
-substring_index(Logins_logs.VMName, '-', -1-3) AS 'Lab Creation (UTC)',
+substring_index(mod_reservations.VMName, '-', -1-3) AS 'Lab Creation (UTC)',
 mod_reservations.Active AS 'Status',
   CONCAT(
     FLOOR(differences.seconds / 3600 / 24), ' days ',
     FLOOR(differences.hours_part / 3600), ' hours ',
     FLOOR(differences.minutes_part / 60), ' minutes ',
-    differences.seconds_part, ' seconds'
-  ) AS 'TotaL Duration',
-   CONCAT(ROUND(((UPTime_Logs.sum_time_deff/differences.seconds)*100), 1),'%') AS 'Active (%)'
-, CONCAT(ROUND(((Logins_logs.sum_time_deff/UPTime_Logs.sum_time_deff)*100), 1),'%') AS 'Login/Active (%)'
-FROM (SELECT t.VMName,SUM(t.Time_Deff) AS 'sum_time_deff'
-		FROM (SELECT t.VMName,
+    differences.seconds_part, 'seconds'
+  ) AS `TotaL Duration`,
+   CONCAT(ROUND(((UPTime_Logs.sum_time_deff/differences.seconds)*100), 1),'%') AS `Powered-on (%)`
+, CONCAT(ROUND(((Logins_logs.sum_time_deff/UPTime_Logs.sum_time_deff)*100), 1),'%') AS `Login/Powered-on (%)`
+FROM (select Distinct VMName, MAX(sum_time_deff) AS sum_time_deff from (SELECT DISTINCT t.VMName,SUM(t.Time_Deff) AS 'sum_time_deff'
+		FROM (SELECT t.VMName AS 'VMName',
        TIMESTAMPDIFF(SECOND, MAX(CASE WHEN action = 'in'  THEN dt end), MIN(CASE WHEN action = 'out' THEN dt end)) AS 'Time_Deff'
-FROM (SELECT t.*,
+			FROM (SELECT t.*,
              (@grp := IF(@VMName = VMName, IF(action = 'in', @grp + 1, @grp ),
                          IF(@VMName := VMName, @grp + 1, @grp + 1)
                         )
              ) AS grp
       FROM (SELECT VMName, Login AS dt, 'in' AS action
             FROM Logins
+            WHERE VMName LIKE CONCAT('%-Persistent-%')
             UNION ALL
             SELECT VMName, Logout, 'out'
             FROM Logouts
@@ -104,8 +105,14 @@ FROM (SELECT t.*,
            (SELECT @VMName := -1, @grp := -1) params
      ) t
 GROUP BY VMName, grp
-ORDER BY VMName, grp) AS t
-GROUP BY VMName) AS Logins_logs,
+ORDER BY VMName, grp
+) AS t
+GROUP BY t.VMName
+UNION
+SELECT DISTINCT VMName, 0 AS 'sum_time_deff' FROM VMUptime_Logs
+GROUP BY VMName) AS t
+GROUP BY t.VMName
+) AS Logins_logs ,
 (SELECT t.VMName, SUM(t.Time_Deff) AS 'sum_time_deff'
 		FROM (SELECT t.VMName,
        TIMESTAMPDIFF(SECOND, MAX(CASE WHEN action = 'in' THEN dt end), MIN(CASE WHEN action = 'out' THEN dt end)) AS 'Time_Deff'
@@ -127,7 +134,7 @@ FROM (SELECT t.*,
 GROUP BY VMName, grp
 ORDER BY VMName, grp) AS t
 GROUP BY VMName) AS UPTime_Logs,
-(SELECT VMName,Start,End, IF(Reservations.End >= UTC_TIMESTAMP(),'Active', 'Deleted') AS active, Lab, Email FROM Reservations) AS mod_reservations,
+(SELECT VMName,substring_index(VMName, '-', -1-3) AS Start,IF(Reservations.End >= UTC_TIMESTAMP(),'Active', 'Deleted') AS active, Lab, Email FROM Reservations WHERE Reservations.VMName LIKE CONCAT('%-Persistent-%')) AS mod_reservations,
 (SELECT
     difference_in_seconds.VMName,
     difference_in_seconds.Start,
@@ -141,15 +148,17 @@ GROUP BY VMName) AS UPTime_Logs,
     mod_reservations.Start AS Start,
     IF(UTC_TIME() < mod_reservations.End, UTC_TIME(), mod_reservations.End) AS End,
     TIMESTAMPDIFF(SECOND, mod_reservations.Start, IF(UTC_TIME() < mod_reservations.End, UTC_TIME(), mod_reservations.End)) AS seconds
-  FROM (SELECT VMName,Start,End FROM Reservations) AS mod_reservations) AS difference_in_seconds) AS differences
-WHERE UPTime_Logs.VMName = Logins_logs.VMName AND
+  FROM (SELECT Reservations.VMName, cast(substring_index(Reservations.VMName, '-', -1-3) as datetime) AS Start,IF(Log IS NULL, END, Log) AS End FROM Reservations LEFT JOIN (SELECT VMName, Log From  VMUptime_Logs WHERE Operation = 'Deleted') AS Deleted_Times ON Deleted_Times.VMName =  Reservations.VMName WHERE Reservations.VMName LIKE CONCAT('%-Persistent-%')) AS mod_reservations) AS difference_in_seconds) AS differences
+WHERE
 mod_reservations.VMName = Logins_logs.VMName AND
+UPTime_Logs.VMName = Logins_logs.VMName AND
 mod_reservations.VMName = UPTime_Logs.VMName AND
 differences.VMName = mod_reservations.VMName AND
-Lab IN ('Appwall') AND
-SUBSTRING_INDEX(mod_reservations.Email, '@', 1) In ('idanso') AND
-mod_reservations.active IN ('Active', 'Inactive')
-GROUP BY UPTime_Logs.VMName;
+Lab IN ('Defense Flow','SSL Inspection','Alteon and Analytics','Alteon Ansible Automation','Alteon GEL Automation','Appwall','KWAF','KWAF - External Authorization Mode','KWAF - Internal Authorization Mode','Virtual DefensePro','Alteon Cloud Controller') AND
+SUBSTRING_INDEX(mod_reservations.Email, '@', 1) In ('anandm','andylau','arunp','danielo','deanm','esteban.pierotti','gregd','haraldb','idanso','jesus.rojas','juann','krishna.gullapalli','maory','massimilianom','noaho','prateek.vishwanath','rajeev.shrestha','ricardom','shaheedb','siddharth.iyer','taly','virg.santos','vladimirv','xiaw','yarivk') AND
+mod_reservations.active IN ('Active', 'Deleted')
+GROUP BY Logins_logs.VMName, mod_reservations.VMName, differences.VMName, UPTime_Logs.VMName, `TotaL Duration`, `Powered-on (%)`, `Login/Powered-on (%)`
+ORDER BY Status;
 
 ########################################################
 #### final query for statistic for Regular (Final) #####
@@ -212,7 +221,8 @@ Logins_logs.VMName NOT LIKE CONCAT('%-Persistent-%') AND
 Lab IN ('Appwall') AND
 SUBSTRING_INDEX(mod_reservations.Email, '@', 1) In ('idanso') AND
 mod_reservations.active IN ('Active', 'Inactive')
-GROUP BY Logins_logs.VMName;
+GROUP BY Logins_logs.VMName
+ORDER BY Status;
  
  
 #########################################
@@ -377,5 +387,10 @@ SELECT
     CAST(@minutes := IF(@diff/60 >= 1, floor((@diff:=@diff-@hours*3600) / 60),0) AS SIGNED), ":", 
     CAST(@diff-@minutes*60 AS SIGNED)) AS 'time';
 
-
-
+SELECT VMName,substring_index(VMName, '-', -1-3) AS Start,
+IF(Reservations.End >= UTC_TIMESTAMP(),End, (SELECT Log From  (SELECT VMName, Log FROM VMUptime_Logs WHERE Operation = 'Deleted' AND VMName = ) AS t WHERE VMName = t.VMName)) AS End,
+ IF(Reservations.End >= UTC_TIMESTAMP(),'Active', 'Deleted') AS active,
+ Lab,
+ Email
+ FROM Reservations
+ WHERE Reservations.VMName LIKE CONCAT('%-Persistent-%')
